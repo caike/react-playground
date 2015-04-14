43fdc1f4522a0ae9484b58e61fa9043612db6d93
Carlos Souza <carloshrsouza@gmail.com>
2015-04-14 07:44:24 -0400

    Add and cross items

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-* npm install
-* npm run watch
+# Todo List
+
+React + ES6 + Class Syntax
+
+## Running
+
+* `npm run watch`
+* Run server, i.e. `python -m SimpleHTTPServer`
+
--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,78 @@
 var React = require("react");
+var cx = require('classnames');
 
-var App = React.createClass({
-    render() {
-        return <h1>Hello change</h1>;
+var allItems = []
+allItems.push("Buy ingredients for Crock Pot");
+allItems.push("Pick up chair at IKEA");
+allItems.push("Go see mom");
+
+class TodoList extends React.Component {
+  constructor(props){
+    super(props);
+    this.addEvent = this.addEvent.bind(this);
+  }
+  getInitialState() {
+    return { allItems };
+  }
+  render() {
+    var items = this.props.items.map((item) => {
+      return <li><TodoItem item={item} /></li>;
+    })
+    return(
+      <div>
+        <ul>{items}</ul>
+        <p><NewTodoItem addEvent={this.addEvent} /></p>
+      </div>
+    );
+  }
+  addEvent(todoItem){
+    allItems.push(todoItem.newItem);
+    this.setState({ allItems });
+  }
+}
+
+class TodoItem extends React.Component {
+  constructor(props){
+    super(props);
+    this.onChangeHandler = this.onChangeHandler.bind(this);
+
+    this.state = {
+      isDone: false
     }
-})
+  }
+  onChangeHandler(e){
+    this.setState({ isDone: e.target.checked });
+  }
+  render(){
+    var classes = cx({
+      'item-done': this.state.isDone
+    });
+    return <div className={classes}>{this.props.item}
+      <input type="checkbox" onChange={this.onChangeHandler} />
+    </div>;
+  }
+}
 
-React.render(<App/>, document.getElementById('example'));
+class NewTodoItem extends React.Component {
+  constructor(props){
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  componentDidMount(){
+    React.findDOMNode(this.refs.itemName).focus();
+  }
+  render(){
+    return (<form onSubmit={this.onSubmit}>
+      <input ref="itemName" type="text" />
+    </form>);
+  }
+  onSubmit(event){
+    event.preventDefault();
+    var input = React.findDOMNode(this.refs.itemName)
+    var newItem = input.value;
+    this.props.addEvent({ newItem });
+    input.value = '';
+  }
+}
+
+React.render(<TodoList items={allItems} />, document.getElementById('example'));
